3e2b560568e0093a8014bac7f5060b6e50580e68
mariabragado <mjbragado@gmail.com>
2022-11-03 10:39:59 +0100

    function improvement

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 import strictEquals from './strictEquals.js';
 
-console.log(['value A', 'value B', 'RESULT']);
-console.log([1, 1, strictEquals(1, 1)]);
-console.log([NaN, NaN, strictEquals(NaN, NaN)]);
-console.log([0, -0, strictEquals(0, -0)]);
-console.log([-0, 0, strictEquals(-0, 0)]);
-console.log([1, '1', strictEquals(1, '1')]);
-console.log([true, false, strictEquals(true, false)]);
-console.log([false, false, strictEquals(false, false)]);
-console.log(['water', 'oil', strictEquals('water', 'oil')]);
+const results = [
+    { a: 1, b: 1, result: strictEquals(1, 1) },
+    { a: NaN, b: NaN, result: strictEquals(NaN, NaN) },
+    { a: 0, b: -0, result: strictEquals(0, -0) },
+    { a: -0, b: 0, result: strictEquals(-0, 0) },
+    { a: 1, b: '1', result: strictEquals(1, '1') },
+    { a: true, b: false, result: strictEquals(true, false) },
+    { a: false, b: false, result: strictEquals(false, false) },
+    { a: 'water', b: 'oil', result: strictEquals('water', 'oil') },
+];
+
+console.log(results);
